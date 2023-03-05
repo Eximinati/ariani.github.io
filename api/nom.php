d36d80ebab71f7fea5ee6c53642d331ff3b5bcd4
Examinati <72595444+Eximinati@users.noreply.github.com>
2023-03-05 14:53:57 +0500

    i didnt buy the domain uk

--- api/nom.php
+++ api/nom.php
@@ -4,7 +4,7 @@ error_reporting(0);
 require __DIR__ . '/request.php';
 header('Content-Type: application/json');
 
-if ($_GET['r'] || $_GET['result']) {
+if($_GET['r'] || $_GET['result']) {
     $result = $_GET['r'] ? $_GET['r'] : $_GET['result'];
 } else {
     $result = 1;
@@ -15,15 +15,15 @@ for ($i = 0; $i < $result; $i++) {
     $request = json_decode($request, true);
     $waifu[] = $request['url'];
 }
-$waifu = preg_replace('/i\.waifu\.pics/', 'i.ariani.my.id', $waifu);
-if ($request == null) {
+$waifu = preg_replace('/i\.waifu\.pics/','i.rei.my.id',$waifu);
+if($request == null) {
     $return = array(
         'apiTimestamp' => time(),
         'apiStatus' => 'error',
         'apiCode' => 500,
         'apiResult' => 'Internal Server Error'
     );
-
+    
     echo json_encode($return, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 } else {
 
@@ -35,6 +35,8 @@ if ($request == null) {
             'url' => $waifu
         )
     );
-
+    
     echo json_encode($return, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 }
+
+
